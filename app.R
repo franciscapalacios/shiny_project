1ab1621698f082f393bffc023dd7cd66731f398c
Francisca Palacios <fpalacios94@gmail.com>
2023-05-26 16:48:59 -0400

    Final Project

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 source("shiny_functions.R")
 library(shinyjs)
 
+linebreaks <- function(n){HTML(strrep(br(), n))}
+
 shinyApp(
   ui = tagList(
     navbarPage("Attrition Clustering",
@@ -8,32 +10,66 @@ shinyApp(
               
                mainPanel(
                  tabsetPanel(
-                   tabPanel("Overview",
-                            h4("Table"),
+                   tabPanel("About",
+                            fluidRow(
+                              column(12, align = "justify", 
+                                     h3("Understanding Employee Attrition"),
+                                     p("Employee attrition, or turnover, is a critical challenge faced by organizations across industries. It is vital to comprehend the underlying patterns and factors contributing to attrition in order to develop effective strategies for employee retention and engagement."),
+                                     br(),
+                                     h3("Going through the app"),
+                                     h5("Exploring the Dataset"),
+                                     p("The app allows you to explore the dataset and gain a comprehensive understanding of the variables and characteristics it encompasses. You can examine employee demographics, job-related attributes, and various factors that may influence attrition. This exploration phase helps you become familiar with the dataset and choose relevant variables for the clustering analysis."),
+                                     br(),
+                                     h5("Identifying Attrition Clusters"),
+                                     p("Once you have explored the dataset, the app guides you through the process of identifying attrition clusters using silhouette plots. Silhouette plots provide a visual representation of the quality and distinctness of each cluster, aiding in the determination of an optimal number of clusters. You can analyze the silhouette scores and select the number of clusters that reveals meaningful patterns and concentrated attrition."),
+                                     br(),
+                                     h5("Unveiling Insights and Decision-Making"),
+                                     p("By uncovering distinct clusters of attrition, this app assists in discovering groups of employees who exhibit similar attrition tendencies. Analyzing these clusters can yield valuable insights into the underlying factors that contribute to attrition within specific employee groups. These insights can inform targeted retention strategies, organizational policies, and initiatives to address attrition and enhance employee satisfaction and engagement."),
+                                     br(),
+                                     h5("References"),
+                                     uiOutput("ref1.link"),
+                                     uiOutput("ref2.link"),
+                                     uiOutput("ref3.link"),
+                                     uiOutput("ref4.link"),
+                                     br()
+                                     
+                              ))
+                   ),
+                 
+                  tabPanel("Overview",
+                            #h4("Table"),
+                            linebreaks(1),
+                            plotOutput("att.perc"),
                             tableOutput("table"),
                             verbatimTextOutput("nulls")
                    ),
                    
                    tabPanel("Correlations",
+                            linebreaks(1),
                             fluidPage(
                               useShinyjs(),
                               selectInput(inputId = "corr_plots",
                                           label = "Choose correlation plot:",
                                           choices = c("Multicollinearity Plot",
                                                       "Correlation Funnel")),
-                              hidden(plotOutput("funnel")),
-                              hidden(plotOutput("corr"))
+                              hidden(plotOutput("funnel",
+                                                height=800, width = 800)),
+                              hidden(plotOutput("corr",
+                                                height=800, width = 800))
                             )
                 
                    ),
                    tabPanel("Explore",
                             fluidPage(
                               useShinyjs(),
+                              linebreaks(1),
                               selectInput(inputId = "explore_plots",
                                           label = "Choose variables:",
                                           choices = setdiff(names(df), "Attrition")),
-                              hidden(plotOutput("continuos_plot")),
-                              hidden(plotOutput("factor_plot"))
+                              hidden(plotOutput("continuos_plot",
+                                                height=500)),
+                              hidden(plotOutput("factor_plot",
+                                                height=500))
                             )
                             )
                  )
@@ -46,8 +82,10 @@ shinyApp(
                plotOutput("sil.plot",
                                     width = "100%"
                ),
+               uiOutput("sil.link"),
                  sliderInput("slider", "Top correlated features to use:", 2, 25, 10),
-                 tableOutput("att.table")
+                 tableOutput("att.table"),
+               verbatimTextOutput("txtout")
                )
       ),
       
@@ -55,17 +93,22 @@ shinyApp(
                mainPanel(
                  tabsetPanel(
                    tabPanel("Review",
-                            plotOutput("k.sil.plot")
+                            linebreaks(1),
+                            plotOutput("k.sil.plot"),
+                            uiOutput("sil2.link")
                             ),
                    
                    tabPanel("Explore", 
                             fluidPage(
                               useShinyjs(),
+                              linebreaks(1),
                               selectInput(inputId = "explore_clusters",
                                           label = "Choose variables:",
                                           choices = setdiff(names(df), "Attrition")),
-                              hidden(plotOutput("continuos_plot2")),
-                              hidden(plotOutput("factor_plot2"))
+                              hidden(plotOutput("continuos_plot2",
+                                                height=500)),
+                              hidden(plotOutput("factor_plot2",
+                                                height=560))
                             )
                             )
                    ))
@@ -94,11 +137,16 @@ shinyApp(
     })
     
     output$txtout <- renderText({
-      paste(input$varType, input$slider/100, sep = ", ")
+      max_row <- sapply(str_extract_all(attr.tbl()$Cluster_Turnover_Rate, '\\d+([.]\\d+)?'), function(x) max(as.numeric(x)))
+      cluster <- as.character(attr.tbl()[which.max(max_row),][1,1])
+      cluster_turnover <- as.character(attr.tbl()[which.max(max_row),][1,2])
+      total_turnover <- as.character(attr.tbl()[which.max(max_row),][1,5])
+      paste("When choosing the", input$slider, "most correlated features, we get", dim(attr.tbl())[1], "clusters. \nCluster", cluster, 
+      "has a turnover rate of", cluster_turnover, "which accounts for", total_turnover, "of the total attrition.")
     })
     
     output$table <- renderTable({
-      head(df)
+      desc.df
     })
     
     output$nulls <- renderText({
@@ -108,6 +156,18 @@ shinyApp(
     output$corr <- renderPlot({
       corrplot(corr.mat, type = "upper", order = "hclust", 
                tl.col = "black", tl.srt = 90)
+    })
+    
+    output$att.perc <- renderPlot({
+      df %>% 
+        group_by(Attrition) %>% 
+        summarise(Percentage=n()/nrow(.)) %>% 
+        ggplot(aes(x="", y=Percentage, fill=Attrition)) +
+        geom_bar(stat="identity", width=1, color="white") +
+        coord_polar("y", start=0) +
+        geom_text(aes(x = 1.1, label = scales::percent(Percentage, accuracy = .1)), 
+                  position = position_stack(vjust = .5), color = "white") +
+        theme_void()
     })
     
     output$funnel <- renderPlot({
@@ -120,7 +180,11 @@ shinyApp(
              aes(x = k, y = sil_width)) +
         geom_point(size = 2) +
         geom_line() +
-        scale_x_continuous(breaks = 2:10)
+        ylab("Silhouette Width") +
+        scale_x_continuous(breaks = 2:10) +
+        theme_minimal() +
+        theme(axis.text=element_text(size=12),
+              axis.title=element_text(size=14))
     })
     
     output$att.table <- renderTable({
@@ -186,6 +250,42 @@ shinyApp(
     output$k.sil.plot <- renderPlot({
       sil = silhouette (cluster.fit()$clustering, gower.dist()) 
       fviz_silhouette(sil)
+    })
+    
+    url <- a("What's a silhouette plot?", href="https://towardsdatascience.com/silhouette-method-better-than-elbow-method-to-find-optimal-clusters-378d62ff6891", 
+             style = "font-size:11px")
+    output$sil.link <- renderUI({
+      tagList(url)
+    })
+    
+    url2 <- a("What's a silhouette plot?", href="https://towardsdatascience.com/silhouette-method-better-than-elbow-method-to-find-optimal-clusters-378d62ff6891", 
+             style = "font-size:11px")
+    output$sil2.link <- renderUI({
+      tagList(url2)
+    })
+    
+    ref1 <- a("A People Analytics Tutorial on Unsupervised Machine Learning", href="https://www.linkedin.com/pulse/people-analytics-tutorial-unsupervised-machine-r-mckinnon-phd-/", 
+              style = "font-size:11px")
+    output$ref1.link <- renderUI({
+      tagList(ref1)
+    })
+    
+    ref2 <- a("Hierarchical Clustering on Categorical Data in R", href="https://towardsdatascience.com/hierarchical-clustering-on-categorical-data-in-r-a27e578f2995", 
+              style = "font-size:11px")
+    output$ref2.link <- renderUI({
+      tagList(ref2)
+    })
+    
+    ref3 <- a("CLUSTERING EXAMPLE: 4 STEPS YOU SHOULD KNOW", href="https://www.datanovia.com/en/blog/clustering-example-4-steps-you-should-know/", 
+              style = "font-size:11px")
+    output$ref3.link <- renderUI({
+      tagList(ref3)
+    })
+    
+    ref4 <- a("How to Use t-SNE Effectively", href="https://distill.pub/2016/misread-tsne/", 
+              style = "font-size:11px")
+    output$ref4.link <- renderUI({
+      tagList(ref4)
     })
     
   }
--- shiny_functions.R
+++ shiny_functions.R
@@ -1,10 +1,30 @@
+library('tidyverse')
+library("GGally")
+library(corrplot)
+library(cluster) 
+library(correlationfunnel)
+library(ggridges)
+library("factoextra")
+
 import.df <- function(){
   df <- read.csv('./HR_Analytics.csv')
   df <- df %>% 
+    select(-c(EmployeeNumber, EmployeeCount, Over18, StandardHours)) %>%
     mutate_if(is.character,as.factor) 
   
   df$BusinessTravel <- factor(df$BusinessTravel, ordered = TRUE, 
                               levels = c("Non-Travel", "Travel_Rarely", "Travel_Frequently"))
+  
+  df$JobLevel <- factor(df$JobLevel, ordered = TRUE)
+  df$PerformanceRating <- factor(df$PerformanceRating, ordered = TRUE)
+  df$WorkLifeBalance <- factor(df$WorkLifeBalance, ordered = TRUE)
+  df$StockOptionLevel <- factor(df$StockOptionLevel, ordered = TRUE)
+  df$RelationshipSatisfaction <- factor(df$RelationshipSatisfaction, ordered = TRUE)
+  df$JobSatisfaction <- factor(df$JobSatisfaction, ordered = TRUE)
+  df$Education <- factor(df$Education, ordered = TRUE)
+  df$EnvironmentSatisfaction <- factor(df$EnvironmentSatisfaction, ordered = TRUE)
+  df$JobInvolvement <- factor(df$JobInvolvement, ordered = TRUE)
+  df$TrainingTimesLastYear <- factor(df$TrainingTimesLastYear, ordered = TRUE)
   
   df <- df %>% 
     mutate(bin.YearsAtCompany = case_when(
@@ -12,11 +32,6 @@ import.df <- function(){
       YearsAtCompany <= 4 ~ 2,
       YearsAtCompany <= 10 ~ 3,
       TRUE ~ 4
-    )) %>% 
-    mutate(num.BusinessTravel = case_when(
-      BusinessTravel == "Non-Travel" ~ 1,
-      BusinessTravel == "Travel_Rarely" ~ 2,
-      TRUE ~ 3
     )) 
   
   return(df)
@@ -26,6 +41,12 @@ import.df <- function(){
 import.corr.mat <- function(df){
   
   cor.mat <- df %>%
+    mutate_at(vars(JobLevel, 
+                   PerformanceRating, WorkLifeBalance, StockOptionLevel, 
+                   RelationshipSatisfaction, JobSatisfaction, JobInvolvement, 
+                   Education, EnvironmentSatisfaction, TrainingTimesLastYear),
+              as.numeric) %>%
+    
   select(Age, DailyRate, DistanceFromHome, Education, 
          EnvironmentSatisfaction, HourlyRate, JobInvolvement, JobLevel, 
          JobSatisfaction, MonthlyIncome, MonthlyRate, 
@@ -33,6 +54,7 @@ import.corr.mat <- function(df){
          RelationshipSatisfaction, StockOptionLevel, TotalWorkingYears, 
          TrainingTimesLastYear, WorkLifeBalance, YearsAtCompany, YearsInCurrentRole, 
          YearsSinceLastPromotion, YearsWithCurrManager) 
+
 
   df.cor = cor(cor.mat)
   return(df.cor)
@@ -42,7 +64,6 @@ import.corr.mat <- function(df){
 import.corr.funnel <- function(df){
   
   corr.tbl.bin <- df %>%
-    select(-EmployeeNumber) %>%
     binarize(n_bins = 4, thresh_infreq = 0.01, name_infreq = "OTHER", one_hot = TRUE)
   
   corr.tbl <- corr.tbl.bin %>%
@@ -69,7 +90,7 @@ import.sil.tbl <- function(gower_dist){
     model$silinfo$avg.width
   })
   
-  sil_tbl <- tibble::tibble(
+  sil_tbl <- tibble(
     k = 2:10,
     sil_width = sil_width
   )
@@ -96,17 +117,18 @@ import.best.k <- function(pam_fit){
   
   attrition_rate_tbl <- cluster_tbl %>%
     
-    dplyr::select(cluster, Attrition) %>%
-    dplyr::mutate(attrition_num = forcats::fct_relevel(Attrition, "No", "Yes") %>% base::as.numeric() - 1) %>%
-    dplyr::group_by(cluster) %>%
-    dplyr::summarise(
+    select(cluster, Attrition) %>%
+    mutate(attrition_num = forcats::fct_relevel(Attrition, "No", "Yes") %>% base::as.numeric() - 1) %>%
+    group_by(cluster) %>%
+    summarise(
       Cluster_Turnover_Rate  = (base::sum(attrition_num) / base::length(attrition_num)) %>% scales::percent(accuracy = 0.1),
       Turnover_Count = base::sum(attrition_num),
       Cluster_Size = base::length(attrition_num)
     ) %>%
-    dplyr::ungroup() %>%
+    ungroup() %>%
     
-    dplyr::mutate(Population_Turnover_Rate = (Turnover_Count / base::sum(Turnover_Count)) %>% scales::percent(accuracy = 0.1))
+    mutate(Population_Turnover_Rate = (Turnover_Count / base::sum(Turnover_Count)) %>% 
+             scales::percent(accuracy = 0.1))
   
   return(attrition_rate_tbl)
 }
@@ -142,7 +164,10 @@ continous.plot <- function(variable){
       size = 4,
       alpha = .2
     ) +
-    coord_cartesian(xlim = c(1.2, NA), clip = "off") 
+    coord_cartesian(xlim = c(1.2, NA), clip = "off") +
+    theme_minimal() +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14))
 
   return(g)
 }
@@ -153,13 +178,18 @@ factor.plot <- function(variable){
   g = df[, c(variable, "Attrition")] %>%
     group_by(pick(everything())) %>%
     summarise(n = n()) %>%
-    mutate(freq = n*100 / sum(n)) %>%
-    ggplot(aes_string(x=variable, y="freq", fill="Attrition")) + 
+    mutate(Percentage = n*100 / sum(n)) %>%
+    ggplot(aes_string(x=variable, y="Percentage", fill="Attrition")) + 
     geom_bar(stat="identity") +
     geom_text(
-      aes(y = freq, label = round(freq,1), group = Attrition),
+      aes(y = Percentage, label = round(Percentage,1), group = Attrition),
       position = position_stack(vjust = .5), color = "white") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    theme(panel.background = element_blank(),
+          axis.ticks.x=element_blank(), axis.text.x=element_blank()) +
+    xlab("") +
+    coord_flip() +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14))
 
   return(g)
 }
@@ -169,11 +199,17 @@ continous.plot2 <- function(df_clusters, variable){
   
   g = ggplot(
     df_clusters, 
-    aes_string(x = variable, y = "as.factor(cluster)", fill = "stat(x)")
+    aes_string(x = variable, y = "as.factor(cluster)", fill = "after_stat(x)")
   ) +
     geom_density_ridges_gradient(scale = 2, size = 0.3, rel_min_height = 0.01) +
     scale_fill_viridis_c(name="", option = "C") +
-    ylab('Cluster')
+    ylab('Cluster') +
+    theme_minimal() +
+    scale_fill_continuous(type = "gradient", trans = 'reverse') +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14)) +
+    #labs(fill = variable) +
+    guides(fill="none")
   
   return(g)
 }
@@ -184,14 +220,22 @@ factor.plot2 <- function(df_clusters, variable){
   g = df_clusters[, c("cluster", variable)] %>%
     group_by(pick(everything())) %>%
     summarise(n = n()) %>%
-    mutate(freq = n*100 / sum(n)) %>%
-    ggplot(aes_string(x=variable, y="freq", fill=variable)) + 
+    mutate(Percentage = n*100 / sum(n)) %>%
+    ggplot(aes_string(x=variable, y="Percentage", fill=variable)) + 
     geom_bar(stat="identity") +
     geom_text(
-      aes(y = freq, label = round(freq,1), group = variable),
+      aes(y = Percentage, label = round(Percentage,1), group = variable),
       position = position_stack(vjust = .5), color = "white") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-    facet_grid(rows = vars(cluster))
+    facet_grid(rows = vars(cluster)) +
+    
+    theme(panel.background = element_blank(),
+          axis.ticks.x=element_blank(), axis.text.x=element_blank()) +
+    coord_flip() +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14)) +
+    scale_fill_brewer(palette = "Dark2") +
+    guides(fill="none") 
   
   return(g)
 }
@@ -205,3 +249,30 @@ import.null.count <- function(df){
 }
 
 
+
+desc.df <- data.frame(Variable = c('Age', 'Attrition', 'BusinessTravel', 'DailyRate',
+                                   'Department', 'DistanceFromHome', 'Education', 'EducationField',
+                                   'EnvironmentSatisfaction', 'Gender', 'HourlyRate', 'JobInvolvement',
+                                   'JobLevel', 'JobRole', 'JobSatisfaction', 'MaritalStatus', 'MonthlyIncome',
+                                   'MonthlyRate', 'NumCompaniesWorked', 'OverTime', 'PercentSalaryHike',
+                                   'PerformanceRating', 'RelationshipSatisfaction', 'StandardHours',
+                                   'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
+                                   'WorkLifeBalance', 'YearsAtCompany', 'YearsInCurrentRole',
+                                   'YearsSinceLastPromotion', 'YearsWithCurrManager'),
+                      
+                      Description = c('The age of the employee. (Numerical)', 'Whether or not the employee has left the organization. (Categorical)',
+                                      'The frequency of business travel for the employee. (Categorical)', 'The daily rate of pay for the employee. (Numerical)',
+                                      'The department the employee works in. (Categorical)', 'The distance from home in miles for the employee. (Numerical)',
+                                      'The level of education achieved by the employee. (Categorical)', "The field of study for the employee's education. (Categorical)",
+                                      "The employee's satisfaction with their work environment. (Categorical)", "The gender of the employee. (Categorical)",
+                                      'The hourly rate of pay for the employee. (Numerical)', "The level of involvement required for the employee's job. (Categorical)",
+                                      "The job level of the employee. (Categorical)", "The role of the employee in the organization. (Categorical)",
+                                      "The employee's satisfaction with their job. (Categorical)", "The marital status of the employee. (Categorical)",
+                                      "The monthly income of the employee. (Numerical)", "The monthly rate of pay for the employee. (Numerical)",
+                                      "The number of companies the employee has worked for. (Numerical)", "Whether or not the employee works overtime. (Categorical)",
+                                      "The percentage of salary hike for the employee. (Numerical)", "The performance rating of the employee. (Categorical)",
+                                      "The employee's satisfaction with their relationships. (Categorical)", "The standard hours of work for the employee. (Numerical)",
+                                      "The stock option level of the employee. (Numerical)", "The total number of years the employee has worked. (Numerical)",
+                                      "The number of times the employee was taken for training in the last year. (Numerical)", "The employee's perception of their work-life balance. (Categorical)",
+                                      "The number of years the employee has been with the company. (Numerical)", "The number of years the employee has been in their current role. (Numerical)",
+                                      "The number of years since the employee's last promotion. (Numerical)", "The number of years the employee has been with their current manager. (Numerical)") )
